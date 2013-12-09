ac1dc789658b66c19d68e97ecff7a5f414a27cf0
fnalin <fnalin@msn.com>
2013-12-09 11:18:16 -0400

    Subindo o Projeto

---
+++ ExFileMVCDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExFileMVCDB.Startup))]
+namespace ExFileMVCDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
